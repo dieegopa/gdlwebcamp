8c3082e3c3b87f3ecc386e3dc14d7c3c087fc36f
Diego Padilla <61509950+dieegopa@users.noreply.github.com>
2020-08-18 21:15:13 -0500

    Update bd_conexion.php

--- GDLWEBCAMP-final/includes/funciones/bd_conexion.php
+++ GDLWEBCAMP-final/includes/funciones/bd_conexion.php
@@ -1,5 +1,5 @@
 <?php 
-    $conexion = new mysqli('localhost','root','root','gdlwebcamp');
+    $conexion = new mysqli('remotemysql.com','9Wn2HYok0g','OAbm3kEWiu','9Wn2HYok0g');
 
     if($conexion->connect_error) {
         echo $error -> $conexion->connect_error;
