e261feae85ce321765d30e11b444f6cea9f6116f
Diego Padilla <61509950+dieegopa@users.noreply.github.com>
2020-08-18 21:22:49 -0500

    Update servicio-registrados.php

--- GDLWEBCAMP-final/admin/servicio-registrados.php
+++ GDLWEBCAMP-final/admin/servicio-registrados.php
@@ -2,14 +2,14 @@
 include_once 'funciones/sesiones.php';
 include_once 'funciones/funciones.php';
 
-$sql = "SELECT fecha_registro, COUNT(*) AS resultado FROM registrados GROUP BY DATE(fecha_registro) ORDER BY fecha_registro";
+$sql = "SELECT DATE(fecha_registro) AS fecha, COUNT(*) AS resultado FROM registrados GROUP BY fecha ORDER BY fecha";
 
 $resultado = $conexion->query($sql);
 
 $arreglo_registros = array();
 
 while($registro_dia = $resultado->fetch_assoc()){
-    $fecha = $registro_dia['fecha_registro'];
+    $fecha = $registro_dia['fecha'];
     $registro['fecha'] = date('y-m-d', strtotime($fecha));
     $registro['cantidad']= $registro_dia['resultado'];
     $arreglo_registros[] = $registro;
