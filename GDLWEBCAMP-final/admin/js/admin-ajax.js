e39204a1f3405bcb304540962add1401ddf71b7c
Diego Padilla <61509950+dieegopa@users.noreply.github.com>
2020-08-18 23:05:33 -0500

    Update admin-ajax.js

--- GDLWEBCAMP-final/admin/js/admin-ajax.js
+++ GDLWEBCAMP-final/admin/js/admin-ajax.js
@@ -11,6 +11,7 @@ $(document).ready(function () {
             dataType: 'json',
             success: function (data) {
                 var resultado = data;
+              console.log(resultado);
                 if (resultado.respuesta == 'exito') {
                     Swal.fire(
                         'Correcto',
