ffd9e79f58c5357fa2ba1e25e97b080142128ac9
Diego Padilla <61509950+dieegopa@users.noreply.github.com>
2020-08-18 22:59:16 -0500

    Update bd_conexion.php

--- GDLWEBCAMP-final/includes/funciones/bd_conexion.php
+++ GDLWEBCAMP-final/includes/funciones/bd_conexion.php
@@ -1,5 +1,10 @@
 <?php 
-    $conexion = new mysqli('remotemysql.com','9Wn2HYok0g','OAbm3kEWiu','9Wn2HYok0g');
+    define( 'DB_USUARIO', '9Wn2HYok0g' );
+    define( 'DB_PASSWORD', 'OAbm3kEWiu' );
+    define( 'DB_HOST', 'remotemysql.com' );
+    define( 'DB_NOMBRE', '9Wn2HYok0g' );
+
+    $conexion = new mysqli( DB_HOST, DB_USUARIO, DB_PASSWORD, DB_NOMBRE );
 
     if($conexion->connect_error) {
         echo $error -> $conexion->connect_error;
