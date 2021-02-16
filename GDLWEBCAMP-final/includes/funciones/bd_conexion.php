41f6ae2a1b431515f4e92778fc34748cf689144b
Diego Padilla <61509950+dieegopa@users.noreply.github.com>
2021-02-16 16:06:01 +0100

    Update bd_conexion.php

--- GDLWEBCAMP-final/includes/funciones/bd_conexion.php
+++ GDLWEBCAMP-final/includes/funciones/bd_conexion.php
@@ -1,10 +1,10 @@
 <?php
-  $host_name = 'sql11.freemysqlhosting.net';
-  $database = 'sql11393353';
-  $user_name = 'sql11393353';
-  $password = 'cV1lAxe2A3';
+  $host_name_db = 'sql11.freemysqlhosting.net';
+  $database_db = 'sql11393353';
+  $user_name_db = 'sql11393353';
+  $password_db = 'cV1lAxe2A3';
 
-  $conexion = new mysqli($host_name, $user_name, $password, $database);
+  $conexion = new mysqli($host_name_db, $user_name_db, $password_db, $database_db);
 
 $conexion->set_charset( 'utf8' );
 
