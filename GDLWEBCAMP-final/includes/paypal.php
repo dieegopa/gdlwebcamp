eb351e49cf5b895127909289e30f8e30bb7dce2f
Diego Padilla <61509950+dieegopa@users.noreply.github.com>
2021-08-29 00:02:38 +0200

    Update paypal.php

--- GDLWEBCAMP-final/includes/paypal.php
+++ GDLWEBCAMP-final/includes/paypal.php
@@ -7,8 +7,7 @@ define('URL_SITIO', 'http://localhost/GDLWEBCAMP/' );
 
 $apiContext = new \PayPal\Rest\ApiContext(
     new \PayPal\Auth\OAuthTokenCredential(
-        'AW59ZHjVDyp6PxEYP9C9t50PMUNafM-xXmjPRTetoXx20UZ-7rP9Ibz1K8TJWkddS6_l_s4rZSeS3GAR',
-        'EGbWhkeNSyHKqsOdduRpKRTU9fG7yXEYCd-4vOVDrvky9I88-uk1I5pGePn7L-aDbwM7Oo9P8XR3END2'
+        '',''
     )
 );
 
